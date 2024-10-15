782889584002272d85b0a84103b7a9aa98eee0ad
achalaidiuk <sasha.treyser90@gmail.com>
2024-10-15 11:01:37 +0300

    fixed flake8

--- planetarium_service/views.py
+++ planetarium_service/views.py
@@ -2,7 +2,6 @@ from django.db.models import Count, F
 from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -23,7 +22,6 @@ from planetarium_service.serializers import (
     AstronomyShowSerializer,
     ReservationSerializer,
     TicketSerializer,
-    PlanetariumDomeImageSerializer,
     PlanetariumDomeDetailSerializer,
     AstronomyShowListSerializer,
     ShowSessionListSerializer,
